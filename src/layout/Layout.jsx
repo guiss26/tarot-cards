c803d07d0511ff489a19b8ec7b2e567c0ef575ce
guiss26 <guissella584@gmail.com>
2025-11-18 22:53:19 -0800

    feat: create Layout.jsx structure

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,1 +1,19 @@
 //Layout.jsx
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+import Box from '@mui/material/Box';
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            <Box sx={{ flex: 1}}>
+                <Outlet />
+            </Box>
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
