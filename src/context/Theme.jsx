ec30f7fe8a8788544b0ee57f2b3ad882bb9fa6b3
guiss26 <guissella584@gmail.com>
2025-11-18 22:50:06 -0800

    feat: configure Theme.jsx to apply fonts for mui-material

---
+++ src/context/Theme.jsx
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Lora, serif",
+    h1: {
+      fontFamily: "Cinzel, serif",
+    },
+    h2: {
+      fontFamily: "Cinzel, serif",
+    },
+    h3: {
+      fontFamily: "Cinzel, serif",
+    },
+    h4: {
+      fontFamily: "Cinzel, serif",
+    },
+    h5: {
+      fontFamily: "Cinzel, serif",
+    },
+    h6: {
+      fontFamily: "Cinzel, serif",
+    },
+  },
+})
+
+export default theme
