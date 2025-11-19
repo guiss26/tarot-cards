f0ea4cded4f2c4a545f4bb8844c8c7f95718d446
guiss26 <guissella584@gmail.com>
2025-11-18 22:48:28 -0800

    feat: configure main.jsx with RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import routerTarot from './router/Router.jsx'
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './context/Theme.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={routerTarot}></RouterProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
