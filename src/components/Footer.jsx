a6a52b63b5c3f3e47920072f98d1334bad6a228d
guiss26 <guissella584@gmail.com>
2025-11-19 20:00:32 -0800

    style: update footer fons-size to 13px

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ import Typography from '@mui/material/Typography';
 const Footer = () => {
     return (
 
-        <Box sx={{ bgcolor: '#1f1f1f', color: '#F8F8F2', p: '50px', textAlign: 'center', marginTop: 'auto'}}>
-            <Typography>Proyecto realizado por Guissella Pérez dentro del Bootcamp de Desarrollo Web Fullstack e iniciación a Devops, P4 FemCoders de Factoría F5</Typography>
-            <Typography>© 2025 TarotApp · Que las cartas te guíen ✨</Typography>
+        <Box sx={{ bgcolor: '#1f1f1f', color: '#F8F8F2', p: '2%', textAlign: 'center', marginTop: 'auto'}}>
+            <Typography sx={{ fontSize: '13px'}}>Proyecto realizado por Guissella Pérez dentro del Bootcamp P4 FemCoders Fullstack, Factoría F5</Typography>
+            <Typography sx={{ fontSize: '13px'}}>© 2025 TarotApp · Que las cartas te guíen ✨</Typography>
         </Box>
     )
 }
