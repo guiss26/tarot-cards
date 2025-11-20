f9b8f1c65492fe95bcc68a7e3ad4c1f4ba7978ac
guiss26 <guissella584@gmail.com>
2025-11-19 18:33:35 -0800

    style: fix footer layout and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 const Footer = () => {
     return (
 
-        <Box sx={{ bgcolor: '#1f1f1f', color: '#F8F8F2', p: '50px', textAlign: 'center'}}>
+        <Box sx={{ bgcolor: '#1f1f1f', color: '#F8F8F2', p: '50px', textAlign: 'center', marginTop: 'auto'}}>
             <Typography>Proyecto realizado por Guissella Pérez dentro del Bootcamp de Desarrollo Web Fullstack e iniciación a Devops, P4 FemCoders de Factoría F5</Typography>
             <Typography>© 2025 TarotApp · Que las cartas te guíen ✨</Typography>
         </Box>
