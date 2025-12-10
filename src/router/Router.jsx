15f805a3f68cc18985563a28b2c9becb49b571b6
guiss26 <guissella584@gmail.com>
2025-12-09 20:57:24 -0800

    fix: configure page routes in Router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,1 +1,27 @@
 //Router.jsx
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App"
+import CardDetailPage from "../pages/CardDetailPage.jsx"
+import Layout from "../layout/Layout"
+import ReadingPage from "../pages/ReadingPage";
+
+const routerTarot = createBrowserRouter([{
+    path: "/",
+    element: <Layout/>,
+    children: [
+        {
+            index: true,
+            element: <App></App>
+        },
+        {
+            path: "/detalle-carta/:id",
+            element: <CardDetailPage></CardDetailPage>
+        },
+        {
+            path: "/lectura-tarot",
+            element: <ReadingPage></ReadingPage>
+        }
+    ]
+}])
+
+export default routerTarot
