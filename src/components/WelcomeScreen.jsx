e584d2cb73f2d350ecc55e94138f908ebfb2530d
guiss26 <guissella584@gmail.com>
2025-12-10 10:51:28 -0800

    feat: add WelcomeScreen component with dynamic message and image that hides after 3 seconds

---
+++ src/components/WelcomeScreen.jsx
@@ -0,0 +1,12 @@
+import Typography from '@mui/material/Typography';
+
+const WelcomeScreen = () => {
+    
+    return (
+        <>
+            <Typography variant='h3' sx={{ my: 2, fontSize: '35px'}}>⚜ Descubre lo que el tarot tiene para ti ⚜</Typography>
+        </>
+    )
+}
+
+export default WelcomeScreen
