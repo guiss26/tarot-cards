24d72106901e318f96f5dac4cf5879cc3c15a0d0
guiss26 <guissella584@gmail.com>
2025-12-11 17:42:45 -0800

    style(layout): add full-page background video

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,47 @@
-//Layout.jsx
+
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Box from '@mui/material/Box';
+import { styled } from '@mui/material';
+
+const VideoContainer = styled(Box)(({ theme }) => ({
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    overflow: "hidden",
+    zIndex: -1,
+}));
+
+const VideoOverlay = styled(Box)(({ theme }) => ({
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    background: "rgba(0, 0, 0, 0.3)",
+}));
+
+const BackgroundVideo = styled("video")(({ theme }) => ({
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+}));
 
 const Layout = () => {
     return (
         <>
             <Navbar />
-            <Box component="main" sx={{ flexGrow: 1, mt: 4}}>
+            <Box component="main" sx={{ flexGrow: 1, mt: 4 }}>
+                <VideoContainer>
+                    <BackgroundVideo autoPlay loop muted playsInline>
+                        <source src="public/cielo_estrellado.mp4" type="video/mp4" />
+                    </BackgroundVideo>
+
+                    <VideoOverlay />
+                </VideoContainer>
                 <Outlet />
             </Box>
             <Footer />
