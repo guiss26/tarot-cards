6c086ddbe73792a2c6791365d6293e8a0ad94eb3
guiss26 <guissella584@gmail.com>
2025-12-12 10:13:45 -0800

    style(footer): darken footer background and reduce opacity

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
-//Footer.jsx
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
 const Footer = () => {
     return (
 
-        <Box sx={{ bgcolor: '#1f1f1f', color: '#F8F8F2', p: '2%', textAlign: 'center', marginTop: 'auto'}}>
+        <Box sx={{ bgcolor: 'rgba(15, 14, 14, 0.7)', color: '#F8F8F2', p: '2%', textAlign: 'center', marginTop: 'auto'}}>
             <Typography sx={{ fontSize: '13px'}}>Proyecto realizado por Guissella Pérez dentro del Bootcamp P4 FemCoders Fullstack, Factoría F5</Typography>
             <Typography sx={{ fontSize: '13px'}}>© 2025 TarotApp · Que las cartas te guíen ✨</Typography>
         </Box>
