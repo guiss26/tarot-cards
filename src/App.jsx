c4490321cc018be8ca6a2fe9e9e7c07ab4763971
guiss26 <guissella584@gmail.com>
2025-12-12 19:11:55 -0800

    feat(app): implement timer to show WelcomeScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
-//Aquí va el home --- cards page
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { Sparkles } from 'lucide-react';
 import WelcomeScreen from './components/WelcomeScreen.jsx';
-// import { createTheme } from "@mui/material/styles";
-// import theme from "./context/Theme.jsx"
 import CardList from './components/CardList.jsx';
-import { Link as RouterLink } from "react-router-dom";
-import { styled } from '@mui/material';
-
-const VideoContainer = styled(Box)(({ theme }) => ({
-  position: "fixed",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  overflow: "hidden",
-  zIndex: -1,
-}));
-
-const VideoOverlay = styled(Box)(({ theme }) => ({
-  position: "absolute",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  background: "rgba(0, 0, 0, 0.3)", // o tu overlay
-}));
-
-const BackgroundVideo = styled("video")(({ theme }) => ({
-  width: "100%",
-  height: "100%",
-  objectFit: "cover",
-}));
 
 function App() {
-  const [count, setCount] = useState(0)
   const [showWelcome, setShowWelcome] = useState(true)
 
   useEffect(() => {
@@ -47,20 +15,16 @@ function App() {
     return () => clearTimeout(timer)
   }, [])
 
+
+  if (showWelcome) {
+    return <WelcomeScreen />;
+  }
+
   return (
     <>
-      {/* Video de fondo global */}
-      {/* <VideoContainer>
-        <BackgroundVideo autoPlay loop muted playsInline>
-          <source src="public/cielo_estrellado.mp4" type="video/mp4" />
-        </BackgroundVideo>
+      <Box sx={{ pt: 4}}>
+        <Typography component="span" sx={{ my: 3, fontSize: '14px', color: '#727572' }}>Selecciona una carta para ver su detalle</Typography>
 
-        <VideoOverlay />
-      </VideoContainer> */}
-
-      <Box>
-        <Typography component="span" sx={{ my: 3, fontSize: '13px', color: '#434543' }}>Selecciona una carta para ver su detalle</Typography>
-        
         <CardList></CardList>
       </Box>
     </>
